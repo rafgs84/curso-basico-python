effa3f149353dbb9e1a3a4d4e9b205ef49c8d201
rafgs84 <rafaelgs84@gmail.com>
2025-11-07 07:37:17 -0300

    Estudo do dia 07/11/2025

--- 07_estrutura_de_dados/listas.py
+++ 07_estrutura_de_dados/listas.py
@@ -24,7 +24,7 @@ import random
 # ITEM 5 - Embaralhando os elementos de uma lista
 # ITEM 6 - Obtendo um elemento aleatório de uma lista
 
-# 04/11/2025
+# 04/11/2025 
 # ITEM 7 - Ordenando os elementos de uma lista
 # ITEM 8 - Removendo elementos de uma lista utilizando o método remove()
 # ITEM 9 - Removendo elementos de uma lista utilizando o método pop()
@@ -32,21 +32,35 @@ import random
 # ITEM 11 - Fazendo cópia de uma lista (Clonar)
 # ITEM 12 - Comparando listas
 
+# 06/11/2025
+# ITEM 13 - Clonando e comparando listas
+# ITEM 14 - Incluindo elementos de uma lista em outra
+# ITEM 15 - Ocorrências de elementos e comprimento da lista
+# ITEM 16 - Menor elemento de uma lista 
+# ITEM 17 - Maior elemento de uma lista 
+# ITEM 18 - Soma dos elementos de uma lista 
+
+# 07/11/2025
+# ITEM 19 - Retornando o índice de um elemento
+# ITEM 20 - Retornando índice e elemento
+# ITEM 21 - Listas aninhadas
+
+
 # ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------ #
 
 ## ITEM 1 - Criação de uma lista vazia
 # Sintaxe básica:   <lista> = []
 # Exemplo:
-# nome_lista = []                                                         # cria uma variável recebendo colchetes vazios. 
-# print(f"Exemplo de Lista vazia: {nome_lista}")                          # imprime o conteúdo da lista
+# nome_lista = []                                                       # cria uma variável recebendo colchetes vazios. 
+# print(f"Exemplo de Lista vazia: {nome_lista}")                        # imprime o conteúdo da lista
 
 # ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------ #
 
 ## ITEM 2 - Criação de uma lista com conteúdo pré-definido
 # Sintaxe básica:   <lista> = [<elemento1>,<elemento2>,<elemento3>,<elementoN>]
 # Exemplo:
-# nome_lista = ["Valor1", "Valor2", "Valor3", "Valor...N"]                # cria uma variável recebendo os valores pré-definidos. 
-# print(f"Exemplo de lista com valores pré-definidos: {nome_lista}")      # imprime o conteúdo da lista
+# nome_lista = ["Valor1", "Valor2", "Valor3", "Valor...N"]              # cria uma variável recebendo os valores pré-definidos. 
+# print(f"Exemplo de lista com valores pré-definidos: {nome_lista}")    # imprime o conteúdo da lista
 
 # ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------ #
 
@@ -54,9 +68,9 @@ import random
 # Método:           append()
 # Sintaxe básica:   <lista>.append(<elemento>)
 # Exemplo:  
-# nome_lista = []                                                         # cria uma variável recebendo colchetes vazios.
-# nome_lista.append("Valor_Metodo_Append")                                # adiciona um novo elemento no final da lista
-# print(f"Exemplo de lista com Append: {nome_lista}")                     # imprime o conteúdo da lista
+# nome_lista = []                                                       # cria uma variável recebendo colchetes vazios.
+# nome_lista.append("Valor_Metodo_Append")                              # adiciona um novo elemento no final da lista
+# print(f"Exemplo de lista com Append: {nome_lista}")                   # imprime o conteúdo da lista
 
 # ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------ #
 
@@ -64,9 +78,9 @@ import random
 # Método:           insert()
 # Sintaxe básica:   <lista>.insert(<índice>,<elemento>)
 # Exemplo: 
-# nome_lista = ["Valor1", "Valor2", "Valor3", "Valor...N"]                # cria uma variável recebendo os valores pré-definidos.
-# nome_lista.insert(2, "Valor_Metodo_Insert")                             # adiciona um novo elemento no índice informado da lista
-# print(f"Exemplo de lista com Insert: {nome_lista}")                     # imprime o conteúdo da lista
+# nome_lista = ["Valor1", "Valor2", "Valor3", "Valor...N"]               # cria uma variável recebendo os valores pré-definidos.
+# nome_lista.insert(2, "Valor_Metodo_Insert")                            # adiciona um novo elemento no índice informado da lista
+# print(f"Exemplo de lista com Insert: {nome_lista}")                    # imprime o conteúdo da lista
 
 # ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------ #
 
@@ -75,9 +89,9 @@ import random
 # Sintaxe básica:   <lista>.shuffle(<lista>)
 # Obs.:         antes de ser executado, necessita que seja importado a biblioteca random.
 # Exemplo:
-# nome_lista = ["Valor1", "Valor2", "Valor3", "Valor...N"]              # cria uma variável recebendo os valores pré-definidos.
-# random.shuffle(nome_lista)                                            # chama o método shuffle da biblioteca random passando a lista como parâmetro. 
-# print(f"Exemplo de lista embaralhada: {nome_lista}")                  # imprime o conteúdo da lista.
+# nome_lista = ["Valor1", "Valor2", "Valor3", "Valor...N"]               # cria uma variável recebendo os valores pré-definidos.
+# random.shuffle(nome_lista)                                             # chama o método shuffle da biblioteca random passando a lista como parâmetro. 
+# print(f"Exemplo de lista embaralhada: {nome_lista}")                   # imprime o conteúdo da lista.
 
 # ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------ #
 
@@ -86,9 +100,9 @@ import random
 # Sintaxe básica:   <lista>.choice(<lista>)
 # Obs.:         antes de ser executado, necessita que seja importado a biblioteca random.
 # Exemplo:
-# nome_lista = ["Valor1", "Valor2", "Valor3", "Valor...N"]              # cria uma variável recebendo os valores pré-definidos.
-# elemento_sorteado = random.choice(nome_lista)                         # cria uma variável que recebe o retorno do método choice da biblioteca random passando a lista como parâmetro
-# print(f"Exemplo de elemento sorteado: {elemento_sorteado}")           # imprimir o elemento sorteado da lista
+# nome_lista = ["Valor1", "Valor2", "Valor3", "Valor...N"]               # cria uma variável recebendo os valores pré-definidos.
+# elemento_sorteado = random.choice(nome_lista)                          # cria uma variável que recebe o retorno do método choice da biblioteca random passando a lista como parâmetro
+# print(f"Exemplo de elemento sorteado: {elemento_sorteado}")            # imprimir o elemento sorteado da lista
 
 # ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------ #
 
@@ -97,13 +111,13 @@ import random
 # Sintaxe básica:   <lista>.sort(reverse = [False | True])
 # Observação:       Caso não informe o parâmetro reverse, a ordenação será crescente.
 # Exemplo:
-# nome_lista = ["Valor5", "Valor3", "Valor4", "Valor1", "Valor2"]         # cria uma variável recebendo os valores pré-definidos.
-# nome_lista.sort(reverse = False)                                      # ordena a lista em ordem crescente
-# print(f"Exemplo de lista ordenada crescente: {nome_lista}")           # imprime os valores da lista
-# nome_lista.sort(reverse = True)                                       # ordena a lista em ordem decrescente
-# print(f"Exemplo de lista ordenada decrescente: {nome_lista}")         # imprime os valores da lista
-# nome_lista.sort()                                                       # teste de ordenação sem a passagem de parâmetro
-# print(nome_lista)                                                       # imprime os valores da lista
+# nome_lista = ["Valor5", "Valor3", "Valor4", "Valor1", "Valor2"]        # cria uma variável recebendo os valores pré-definidos.
+# nome_lista.sort(reverse = False)                                       # ordena a lista em ordem crescente
+# print(f"Exemplo de lista ordenada crescente: {nome_lista}")            # imprime os valores da lista
+# nome_lista.sort(reverse = True)                                        # ordena a lista em ordem decrescente
+# print(f"Exemplo de lista ordenada decrescente: {nome_lista}")          # imprime os valores da lista
+# nome_lista.sort()                                                      # teste de ordenação sem a passagem de parâmetro
+# print(nome_lista)                                                      # imprime os valores da lista
 
 # ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------ #
 
@@ -113,7 +127,7 @@ import random
 # Observação:       Deveremos informar o conteúdo do elemento
 # Exemplo:
 # nome_lista = ["Valor5", "Valor3", "Valor4", "Valor1", "Valor2"]         # cria uma variável recebendo os valores pré-definidos.
-# nome_lista.remove("Valor4")                                            # remove o elemento "Valor4"
+# nome_lista.remove("Valor4")                                             # remove o elemento "Valor4"
 # print(nome_lista)                                                       # imprime os valores da lista
 
 # ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------ #
@@ -163,21 +177,99 @@ import random
 # lista3 = [1,5,3,4,2]
 # lista4 = [1,2,3,4]
 
-# #Verificar se a lista1 e lista2 são iguais
+## Verificar se a lista1 e lista2 são iguais
 # if lista1 == lista2:
 #     print("As listas 1 e 2 são iguais.")
 # else:
 #     print("As listas 1 e 2 são diferentes.")
 
-# #Verificar se a lista2 e lista3 são iguais
-# if lista2 = lista3:
+## Verificar se a lista2 e lista3 são iguais
+# if lista2 == lista3:
 #     print("As listas 2 e 3 são iguais.")
 # else:
 #     print("As listas 2 e 3 são diferentes.")
 
-# #Verificar se a lista3 e lista4 são iguais
+## Verificar se a lista3 e lista4 são iguais
 # if lista2 == lista3:
 #     print("As listas 3 e 4 são iguais.")
 # else:
 #     print("As listas 3 e 4 são diferentes.")
+
+# ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------ #
+
+# Incluir o conteúdo dos estudos abaixo:
+
+# 06/11/2025
+# ITEM 13 - Clonando e comparando listas
+# ITEM 14 - Incluindo elementos de uma lista em outra
+# ITEM 15 - Ocorrências de elementos e comprimento da lista
+# ITEM 16 - Menor elemento de uma lista 
+# ITEM 17 - Maior elemento de uma lista 
+# ITEM 18 - Soma dos elementos de uma lista 
+
+
+# ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------ #
+
+## ITEM 19 - Retornando o índice de um elemento
+# Função:           index()
+# Sintaxe básica:   <lista>.index(<elemento>)
+# Observação:       Retorna a primeira ocorrência do elemento informado.
+# Exemplo:
+# lista = [1,2,3,4,5]
+# procurar_por = 3
+# indice = lista.index(procurar_por)
+# print(indice)
+
+# Demostração manual da mesma operação sem usar a função
+# lista = [1,2,3,4,5,2]
+# procurar_por = 2
+# for indice, valor in enumerate(lista):
+#     print(f"Teste do índice {indice}")
+#     if valor == procurar_por:
+#         print(indice)
+#         break
+
+# ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------ #
+
+## ITEM 20 - Retornando índice e elemento
+# Função:           index()
+# Sintaxe básica:   <lista>.index(<elemento>)
+# Observação:       
+# Exemplo:
+# lista = ["A","B","C","D"]
+# for chave, valor in enumerate(lista): 
+#     print(f"Posição: {chave} Valor: {valor}")
+
+# ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------ #
+
+## ITEM 21 - Listas aninhadas
+# Função:           index()
+# Sintaxe básica:   <lista>.index(<elemento>)
+# Observação:       
+# Exemplo:
+# lista = ["A","B",["C","D"],["E","F"]]
+# print(lista[0]) # -> VALOR: "A"
+
+lista = ["A","B","C","D",["E","F"]]
+procurar_por = "E"
+# Cria um range de 0 até o tamanho da lista, e faz um loop atribuindo a variável indiceLista o valor do range atual.
+for indiceLista in range(0, len(lista)): 
+    # Imprime o conteúdo do elemento da lista na posição indicada pela variável indiceLista.
+    print(f"Índice lista: {indiceLista} - Valor: {lista[indiceLista]}") 
+    # Verifica na lista se o conteúdo do elemento na posição indicada pela variável indiceLista é igual ao conteúdo da variável procurar_por
+    if lista[indiceLista] == procurar_por:
+        # Se for igual irá imprimir no terminal a mensagem "Achou". 
+        print("Achou") 
+    # Se a verificação do if for falsa, ele vai verificar se o elemento na posição indicada pela variável indiceLista é uma instância da classe LIST.
+    elif isinstance(lista[indiceLista], list): 
+        # Cria um range de 0 até o tamanho do elemento na posição indicada pela variável indiceLista, e faz um loop atribuindo a variável indiceSubLista o valor do range atual. 
+        for indiceSubLista in range(0, len(lista[indiceLista])):
+            # Imprimi a mensagem abaixo informando o índice da lista externa, o índice da lista interna e o conteúdo da lista interna 
+            print(f"Índice lista: {indiceLista} - Índice sub-lista: {indiceSubLista} Valor: {lista[indiceLista][indiceSubLista]}")
+            # Verificar se o conteúdo da lista interna é igual ao conteúdo da variável procurar_por.
+            # Para acessar o conteúdo da sub-lista é necessário informar o índice da lista externa e o índice da lista interna onde o conteúdo está localizado. Exemplo: lista[indiceListaExterna][indiceListaInterna]
+            if lista[indiceLista][indiceSubLista] == procurar_por:
+                # Imprimir a mensagem "Achou"
+                print("Achou")
+
 
